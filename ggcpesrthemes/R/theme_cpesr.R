5fd90095eea5a392a4376b3b7f8d905cdf7fdcef
juliengossa <julien.gossa@gmail.com>
2022-01-03 00:33:04 +0100

    Nouvelle version

--- ggcpesrthemes/R/theme_cpesr.R
+++ ggcpesrthemes/R/theme_cpesr.R
@@ -12,14 +12,14 @@
 #' @examples
 theme_cpesr <- function(base_family="Raleway", ...) {
   return(
-    ggthemes::theme_hc(base_family=base_family, ...)
+    ggthemes::theme_hc(base_family=base_family, ...) + ggplot2::theme(axis.title.y = element_text(angle=90))
   )
 }
 
 
-#' theme_cpesr
+#' cpesr_cap
 #'
-#' GGploth theme for the CPESR, adding a caption
+#' Caption for CPESR
 #'
 #' @param authors a set of author names
 #' @param camille add Camille Noûs as authors (default: TRUE)
@@ -27,7 +27,6 @@ theme_cpesr <- function(base_family="Raleway", ...) {
 #' @param licence the licence (default: GPL-3.0)
 #' @param source the source of data
 #' @param base_family the font base family (default: Raleway)
-#' @param ... others arguments passed to theme_hc
 #'
 #' @return
 #' @export
@@ -35,25 +34,24 @@ theme_cpesr <- function(base_family="Raleway", ...) {
 #' @examples
 #' ggplot(kpiESR::esr, aes(x=Rentrée, y=kpi.ETU.P.effectif, fill=Type)) +
 #' geom_boxplot() +
-#' theme_cpesr(authors=c("Author One","Author Two"),
-#'             camille=FALSE,
-#'             licence="CC BY-SA",
-#'              url="https://github.com/cpesr/cpesrthemes" )
-theme_cpesr_cap <- function(authors=NULL,
-                            camille=NULL,
-                            url=NULL,
-                            licence=NULL,
-                            source=NULL,
-                            base_family=NULL,
-                            ...) {
-
+#' cpesr_cap(authors=c("Author One","Author Two"),
+#'           camille=FALSE,
+#'           licence="CC BY-SA",
+#'           url="https://github.com/cpesr/cpesrthemes" )
+cpesr_cap <- function(authors=NULL,
+                      camille=NULL,
+                      url=NULL,
+                      licence=NULL,
+                      source=NULL,
+                      base_family=NULL) {
+  
   if(is.null(authors)) authors <- .theme_cpesr.env$authors
   if(is.null(camille)) camille <- .theme_cpesr.env$camille
   if(is.null(url)) url <- .theme_cpesr.env$url
   if(is.null(licence)) licence <- .theme_cpesr.env$licence
   if(is.null(source)) source <- .theme_cpesr.env$source
   if(is.null(base_family)) base_family <- .theme_cpesr.env$base_family
-
+  
   if(camille) { authors <- c(authors, "Camille Noûs") }
   captxt = paste(
     paste(na.omit(authors), collapse=", "),
@@ -61,19 +59,48 @@ theme_cpesr_cap <- function(authors=NULL,
     licence,
     url,
     ifelse(!is.na(source),paste0("\nSource : ", source),"") )
+  
+  return(list(labs(caption = captxt)))
+}
 
-
+#' theme_cpesr_cap
+#'
+#' ggplot theme for the CPESR, adding a caption
+#'
+#' @param authors a set of author names
+#' @param camille add Camille Noûs as authors (default: TRUE)
+#' @param url a reference url (default: cpesr.fr)
+#' @param licence the licence (default: GPL-3.0)
+#' @param source the source of data
+#' @param base_family the font base family (default: Raleway)
+#' @param ... other arguments passed to theme_hc
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' ggplot(kpiESR::esr, aes(x=Rentrée, y=kpi.ETU.P.effectif, fill=Type)) +
+#' geom_boxplot() +
+#' theme_cpesr_cap()
+#' 
+theme_cpesr_cap <- function(authors=NULL,
+                            camille=NULL,
+                            url=NULL,
+                            licence=NULL,
+                            source=NULL,
+                            base_family=NULL,
+                            ...) {
   return(
     list(
       theme_cpesr(base_family=base_family, ...),
-      labs(caption = captxt)
+      cpesr_cap(authors,camille,url,licence,source,base_family)
     )
   )
 }
 
 #' theme_cpesr_setup
 #'
-#' GGploth theme for the CPESR, adding a caption
+#' setting up global values for the CPESR theme
 #'
 #' @param authors a set of author names
 #' @param camille add Camille Noûs as authors (default: TRUE)
@@ -81,7 +108,6 @@ theme_cpesr_cap <- function(authors=NULL,
 #' @param licence the licence (default: LO 2.0)
 #' @param source the source of data
 #' @param base_family the font base family (default: Raleway)
-#' @param ... others arguments passed to theme_hc
 #'
 #' @return
 #' @export
